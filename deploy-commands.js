aabc9d741ba2f8bd23ad70eb25ac56eb4e3539bc
anshalan <jedrzejczyk.pawel@outlook.com>
2023-12-09 02:20:12 +0100

    fix deploy

--- deploy-commands.js
+++ deploy-commands.js
@@ -2,6 +2,8 @@ const { REST, Routes } = require('discord.js');
 //const { clientId, guildId, token } = require('./config.json');
 const fs = require('node:fs');
 const path = require('node:path');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const commands = [];
 // Grab all the command folders from the commands directory you created earlier
@@ -44,4 +46,4 @@ const rest = new REST().setToken(process.env.token);
 		// And of course, make sure you catch and log any errors!
 		console.error(error);
 	}
-});
+})();
